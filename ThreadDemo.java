6a30159083de524eb7bb42509f117c3109a2f093
mynks44 <suranimayank44@gmail.com>
2024-07-12 18:40:48 -0400

    threadss

---
+++ ThreadDemo.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread {
+    private String threadName;
+
+    MyThread(String name) {
+        threadName = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + ": " + i);
+            try {
+                Thread.sleep(1000); // Sleep for 1 second
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread-1");
+        MyThread t2 = new MyThread("Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
