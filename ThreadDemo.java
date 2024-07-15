3a1c7bbf674804b931dc5698d6166c1769b1850d
mynks44 <suranimayank44@gmail.com>
2024-07-15 00:10:13 -0400

    updateddd

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -9,7 +9,26 @@ class MyThread extends Thread {
         for (int i = 1; i <= 5; i++) {
             System.out.println(threadName + ": " + i);
             try {
-                Thread.sleep(1000); // Sleep for 1 second
+                Thread.sleep(500); // Sleep for 0.5 seconds
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class MyRunnable implements Runnable {
+    private String threadName;
+
+    MyRunnable(String name) {
+        threadName = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + ": " + i);
+            try {
+                Thread.sleep(500); // Sleep for 0.5 seconds
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
@@ -19,10 +38,19 @@ class MyThread extends Thread {
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        MyThread t1 = new MyThread("Thread-1");
-        MyThread t2 = new MyThread("Thread-2");
+        MyThread t1 = new MyThread("Thread-1 (extends Thread)");
+        Thread t2 = new Thread(new MyRunnable("Thread-2 (implements Runnable)"));
 
         t1.start();
         t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Both threads have finished execution.");
     }
 }
