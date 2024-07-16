24725610bf913084a541bfd12b6bcf66535725c2
mynks44 <suranimayank44@gmail.com>
2024-07-16 00:36:17 -0400

    Linearsearch

---
+++ LinearSearch.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int key = 7;
+        int result = linearSearch(arr, key);
+        System.out.println("Key found at index: " + result);
+    }
+}
